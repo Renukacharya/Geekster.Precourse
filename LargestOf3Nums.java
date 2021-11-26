81e8deec6d51b50833d9de986035888a74c347b6
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2021-11-26 17:35:13 +0530

    Create LargestOf3Nums.java

---
+++ LargestOf3Nums.java
@@ -0,0 +1,16 @@
+/* created by @Charya using "?"" operator
+to find the largest number out of 3 numbers given
+ */
+ 
+public class LargestOf3Nums {
+ 
+    public static void main(String[] args) {
+        int a = 198;
+        int b = 82;
+        int c = 389;
+         
+        int largest = (a>b)?(a>c?a:c):(b>c?b:c);
+         
+        System.out.print(largest + " is the largest.");
+    }
+}
