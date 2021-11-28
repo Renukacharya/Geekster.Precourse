eb61a7c4618ed8bc389be6351f51b7b23b15713f
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2021-11-28 14:39:58 +0530

    Create GreatestOf3.java

---
+++ GreatestOf3.java
@@ -0,0 +1,14 @@
+public class GreatestOf3 {
+	public static void main(String args[]) {
+	int a=125;
+	int b=225;
+	int c=345;
+	if(a>b && a>c) {
+		System.out.println(a);
+	}else if(b>c && b>1){
+		System.out.println(b);
+	}else
+		System.out.println(c);
+	}
+		
+	}
