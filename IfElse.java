69f21d0a34e7b56d4911ce9c47a2845247cbb13d
Mr. Developer <31615147+Renukacharya@users.noreply.github.com>
2021-11-28 14:31:18 +0530

    Create IfElse.java

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+//A Java Program to demonstrate the use of if-else statement.  
+//It is a program of odd and even number.  
+public class IfElseExample {  
+public static void main(String[] args) {  
+     
+    int number=13;  
+    
+    if(number%2==0){  
+        System.out.println("even number");  
+    }else{  
+        System.out.println("odd number");  
+    }  
+}  
+}  
